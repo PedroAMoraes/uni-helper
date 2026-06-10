ee24d2bd014283c8d2a1b501bb492d59a8b42b97
PedroAMoraes <pealcantara@hotmail.com>
2026-06-10 08:47:01 -0300

    Troca de listview por recyclerview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
     implementation(libs.material)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)

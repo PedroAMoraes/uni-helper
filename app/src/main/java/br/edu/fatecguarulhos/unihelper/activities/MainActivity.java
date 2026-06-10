c3c901bcdb33b0a7d04285b6938f4fbd7ef7beed
PedroAMoraes <pealcantara@hotmail.com>
2026-06-10 13:18:37 -0300

    Atualizar materias sempre que voltar a tela inicial

--- app/src/main/java/br/edu/fatecguarulhos/unihelper/activities/MainActivity.java
+++ app/src/main/java/br/edu/fatecguarulhos/unihelper/activities/MainActivity.java
@@ -94,4 +94,9 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        exibirMaterias();
+    }
 }
